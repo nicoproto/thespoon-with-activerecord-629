60d70b3b1aafaf45248d2e37c357f574db4119dc
Nico Proto <nicolasproto@gmail.com>
2021-06-01 11:24:08 +0200

    Finished lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CREATE
+  # A form to add the restaurant information ✅
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # Create the restaurant ✅
+  post "/restaurants", to: "restaurants#create"
+
+  # READ
+  # Read all the restaurants ✅
+  get "/restaurants", to: "restaurants#index"
+  # Read one specific restaurant ✅
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # UPDATE
+  # form for the resto fields✅
+  get "/restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # Update the restaurant✅
+  patch "/restaurants/:id", to: "restaurants#update"
+
+  # DELETE ✅
+  delete "/restaurants/:id", to: "restaurants#destroy"
+
+
+  # or
+  # resources :restaurants
+
+  # resources :restaurants, only: [:index, :show ]
 end
